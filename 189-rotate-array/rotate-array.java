f11395523db0e1858532a63418e90b044df44757
Harun Matthew <harunmatthewb@gmail.com>
2026-04-21 11:22:01 +0530

    Time: 7 ms (3.88%) | Memory: 64.1 MB (8.60%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k=k%nums.length;
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i:nums){
+            list.add(i);
+
+        }
+        Collections.rotate(list,k);
+        for(int i=0;i<nums.length;i++){
+            nums[i]=list.get(i);
+        }
+    }
+}
