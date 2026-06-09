f8a7119b57e1c1fa24446dbf61649f67433fb49c
Harun Matthew <harunmatthewb@gmail.com>
2026-06-09 10:36:54 +0530

    Time: 20 ms (5.01%) | Memory: 48.2 MB (6.69%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int candy(int[] arr) {
+       int left[]=new int[arr.length];
+       int right[] = new int[arr.length];
+        int sum=0;
+        left[0]=1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i-1]<arr[i]){
+                left[i]=left[i-1]+1;
+            }
+            else{
+                left[i]=1;
+            }
+        }
+        System.out.println(Arrays.toString(left));
+        right[arr.length-1]=1;
+        for(int i=arr.length-2;i>=0;i--){
+            if(arr[i]>arr[i+1]){
+                
+                right[i]=right[i+1]+1;
+            }
+            else{
+                right[i]=1;
+                
+            }
+        }
+        System.out.println(Arrays.toString(right));
+        int tot[]=new int[arr.length];
+
+        for(int i=0;i<arr.length;i++){
+            tot[i]=Math.max(left[i],right[i]);
+        }
+        System.out.println(Arrays.toString(tot));
+        for(int i:tot){
+            sum=sum+i;
+        }
+        return sum;
+
+    }
+}
