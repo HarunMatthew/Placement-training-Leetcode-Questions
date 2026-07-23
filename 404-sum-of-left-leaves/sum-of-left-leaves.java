7176b054cacd310e0531c7a958027157b79078b7
Harun Matthew <harunmatthewb@gmail.com>
2026-07-23 17:33:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (29.93%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum=0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        
+        height(root);
+        return sum;
+    }
+    private int height(TreeNode root){
+        if(root== null){
+            return 0;
+        }
+        if(root.left!=null && root.left.left==null && root.left.right==null){
+            sum=sum+root.left.val;
+        }
+        height(root.left);
+        height(root.right);
+        return sum;
+    }
+
+}
