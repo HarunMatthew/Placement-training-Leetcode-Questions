205726b98c26d2414a583859255c95b4fc6669da
Harun Matthew <harunmatthewb@gmail.com>
2026-07-30 15:25:00 +0530

    Time: 6 ms (99.27%) | Memory: 49.5 MB (72.07%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs==null || strs.length==0){
+            return new ArrayList<>();
+        }
+
+        HashMap<String,List<String>> map=new HashMap<>();
+
+        for(String i : strs){
+            char[] ch=i.toCharArray();
+            Arrays.sort(ch);
+            String s=new String(ch);
+
+            if(!map.containsKey(s)){
+                map.put(s,new ArrayList<>());
+            }
+            map.get(s).add(i);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
