307d452e609f7be229e460ed6c20e5c0690dfdef
Harun Matthew <harunmatthewb@gmail.com>
2026-07-30 16:12:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (28.23%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int sum=0;
+        int n=word.length();
+
+        for(int i=0;i<n;i++){
+            sum=sum+(i/8)+1;
+        }
+        return sum;
+    }
+}
