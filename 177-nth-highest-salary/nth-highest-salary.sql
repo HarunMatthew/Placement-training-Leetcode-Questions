853c54d8bae4892fd99bab5e958e47cb3e3fd887
Harun Matthew <harunmatthewb@gmail.com>
2026-08-05 11:59:52 +0530

    Time: 704 ms (7.76%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,7 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+        select max(salary) from (select salary,dense_rank() over (order by salary desc) as ran from employee)e where ran=n
+  );
+END
