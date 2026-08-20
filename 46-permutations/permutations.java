d85b6b1d1958cff4c38a08d85371b0229ef8a39a
Harun Matthew <harunmatthewb@gmail.com>
2026-08-20 10:12:58 +0530

    Time: 1 ms (87.45%) | Memory: 45.4 MB (79.04%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> result=new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         backtrack(result,new ArrayList<>(),nums,new boolean[nums.length]);
         return result;
     }
-    private void backtrack(List<List<Integer>> result,List<Integer> curr,int[] nums,boolean[] used){
+    public void backtrack(List<List<Integer>> result,List<Integer> curr,int[] nums,boolean[] visited){
         if(curr.size()==nums.length){
             result.add(new ArrayList<>(curr));
             return;
         }
+        
         for(int i=0;i<nums.length;i++){
-            if(used[i]){
+            if(visited[i]){
                 continue;
             }
-            used[i]=true;
+            visited[i]=true;
             curr.add(nums[i]);
-            backtrack(result,curr,nums,used);
-           curr.remove(curr.size()-1);
-           used[i]=false;
+            backtrack(result,curr,nums,visited);
+            curr.remove(curr.size()-1);
+            visited[i]=false;
         }
     }
 }
